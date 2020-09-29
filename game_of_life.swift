d483050228e045f612b93cf6e7acf077abcc64d6
z4yross <z4yross@gmail.com>
2020-09-29 13:25:28 -0500

    added mouse input

--- game_of_life.swift
+++ game_of_life.swift
@@ -4,16 +4,22 @@
 import Foundation
 
 
-let len_grid = 1000
+let len_grid = 500
 var arr = Array(repeating: Array(repeating: 0, count: len_grid), count: len_grid)
 var arr1 = Array(repeating: Array(repeating: 0, count: len_grid), count: len_grid)
 
 var ex = 1
 var ey = 1
 
-arr[100][100] = 1
-arr[100][101] = 1
-arr[100][102] = 1
+var leftButton = UInt32(GLFW_PRESS)
+var rightButton = UInt32(GLFW_PRESS)
+
+var begin = false
+
+var cursorX: Double = 0
+var cursorY: Double = 0
+
+let windowlen = 1000
 
 func setup(){
     glEnable(UInt32(GL_BLEND))
@@ -33,6 +39,19 @@ func draw(){
 
     glColor4f(1.0, 1.0, 1.0, 1)
 
+    if (rightButton == UInt32(GLFW_PRESS)){
+        begin = !begin
+    }
+
+    if(leftButton == UInt32(GLFW_PRESS)){
+        let posx = Int(cursorX / Double(windowlen) * Double(len_grid))
+        let posy = len_grid - Int(cursorY / Double(windowlen) * Double(len_grid))
+
+        if(posx < len_grid && posx >= 0 && posy < len_grid && posy > 0){
+            arr[posx][posy] = arr[posx][posy] == 0 ? 1 : 0
+        }            
+    }
+
 
     for i in 1...arr.count - 2 {
         for j in 1...arr[0].count - 2 {
@@ -41,24 +60,27 @@ func draw(){
                 sqr(x: Float(i), y: Float(j), grid_len: len_grid)
             }
 
+            if (begin){
+                var neib = 0
 
-            var neib = 0
+                neib = arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1] + arr[i][j-1] + arr[i][j+1] + arr[i+1][j-1] + arr[i+1][j] + arr[i+1][j+1]
 
-            neib = arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1] + arr[i][j-1] + arr[i][j+1] + arr[i+1][j-1] + arr[i+1][j] + arr[i+1][j+1]
-
-            if (arr[i][j] == 0){
-                if (neib == 3){
-                    arr1[i][j] = 1 
+                if (arr[i][j] == 0){
+                    if (neib == 3){
+                        arr1[i][j] = 1 
+                    }
                 }
-            }
-            else{
-                arr1[i][j] = (neib == 3 || neib == 2) ? 1 : 0
+                else{
+                    arr1[i][j] = (neib == 3 || neib == 2) ? 1 : 0
+                }
             }
         }
     }
 
-    arr = arr1
-    arr1 = Array(repeating: Array(repeating: 0, count: len_grid), count: len_grid) 
+    if(begin){
+        arr = arr1
+        arr1 = Array(repeating: Array(repeating: 0, count: len_grid), count: len_grid) 
+    }
 }
 
 func line(x: Float, y: Float, x1: Float, y1: Float){
@@ -95,15 +117,12 @@ func sqr(x: Float, y: Float, grid_len: Int){
 
 
 
-
-
-
 if(glfwInit() == 0) {
     print("Failed to initialize GLFW! I'm out!")
     exit(-1)
 }
 
-guard let window = glfwCreateWindow(2000, 2000, "OpenGL test - Swift", nil, nil)
+guard let window = glfwCreateWindow(Int32(windowlen), Int32(windowlen), "OpenGL test - Swift", nil, nil)
 else {
     print("Failed to open a window! I'm out!")
     glfwTerminate()
@@ -112,10 +131,23 @@ else {
 
 glfwMakeContextCurrent(window)
 
+
+if (glfwRawMouseMotionSupported() == 0){
+    glfwSetInputMode(window, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE)
+}
+
+glfwGetCursorPos(window, &cursorX, &cursorY)
+
 setup()
 
 while (glfwWindowShouldClose(window) == 0) {
+    leftButton = UInt32(glfwGetMouseButton(window, Int32(GLFW_MOUSE_BUTTON_LEFT)))
+    rightButton = UInt32(glfwGetMouseButton(window, Int32(GLFW_MOUSE_BUTTON_RIGHT)))
+
+    glfwGetCursorPos(window, &cursorX, &cursorY)
+
     draw()
+    
 
     glfwSwapBuffers(window)
     glfwPollEvents()
